b06c13349f6942a7f7ec24140cbed272cba26c10
Nwozor Christopher <christophernwozor@gmail.com>
2025-04-22 12:49:45 +0100

    first commit

---
+++ app/components/Contact.jsx
@@ -0,0 +1,127 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React, { useState } from "react";
+import { motion } from "motion/react";
+
+const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Submitting...");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "a20c85cf-4e3b-4d5c-a697-732df80bec45");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="contact"
+      className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto] dark:bg-none'
+    >
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="text-center mb-2 text-lg font-Ovo"
+      >
+        Contact Me
+      </motion.h4>
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="text-center text-5xl font-Ovo"
+      >
+        Get In Touch
+      </motion.h2>
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+        className="mt-5 mb-12 font-Ovo text-center  max-w-2xl mx-auto"
+      >
+        I'd be grateful to hear from you. Whether you have a question, a project
+        in mind, feedback, or comments, please feel free to reach out. I am
+        always open to discussing new projects, creative ideas, or
+        opportunities.
+      </motion.p>
+      <motion.form
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.9 }}
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto"
+      >
+        <div className="grid grid-cols-auto gap-6 mb-8">
+          <motion.input
+            initial={{ x: -50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.1 }}
+            type="text"
+            placeholder="Please Enter Your Name"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90"
+            name="name"
+          />
+          <motion.input
+            initial={{ x: 50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.2 }}
+            type="email"
+            placeholder="Please Enter Your Email"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90"
+            name="email"
+          />
+        </div>
+        <div>
+          <motion.textarea
+            initial={{ y: 100, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.3 }}
+            placeholder="Please Enter Your Message"
+            required
+            className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90"
+            name="message"
+          ></motion.textarea>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.3 }}
+            type="submit"
+            className="py-3 px-8 w-max flex justify-between items-center gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 cursor-pointer dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover"
+          >
+            Submit
+            <Image
+              src={assets.right_arrow_white}
+              alt="right arrow"
+              className="w-4"
+            />
+          </motion.button>
+
+          <p className="mt-5">{result}</p>
+        </div>
+      </motion.form>
+    </motion.div>
+  );
+};
+
+export default Contact;
